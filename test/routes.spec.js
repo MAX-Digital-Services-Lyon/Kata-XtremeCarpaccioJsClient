16b99a732287c29928a0046c6835bcd8e296d085
Kevin Delfour <kevin.delfour@maxds.fr>
2023-12-21 14:55:55 +0100

    :feat: Remove default example response

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -17,15 +17,15 @@ describe('routes.js tests', function () {
       order(req, res, function () { });
     });
 
-    it('should return an total object when received a body', function () {
-      const req = { body: { prices: [1.0], quantities: [1], country: "FR", reduction: "STANDARD" } };
-      const res = {
-        json: function (data) {
-          assert.deepStrictEqual(data, { total: 1234.56 });
-        }
-      };
-      order(req, res, function () { });
-    });
+    // it('should return an total object when received a body', function () {
+    //   const req = { body: { prices: [1.0], quantities: [1], country: "FR", reduction: "STANDARD" } };
+    //   const res = {
+    //     json: function (data) {
+    //       assert.deepStrictEqual(data, { total: 1234.56 });
+    //     }
+    //   };
+    //   order(req, res, function () { });
+    // });
   });
 
   describe('feedback function', function () {
